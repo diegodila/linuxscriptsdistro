a2bcd18a5f613f3fef6b08a32c77f4172304e2df
Diego <ferreira.dfg@gmail.com>
2021-03-19 12:57:32 -0300

    add github repositorys

--- installationdistro/install-artix.sh
+++ installationdistro/install-artix.sh
@@ -20,10 +20,23 @@ git config --global credential.helper 'cache --timeout=172800'
 mkdir $HOME/Projects
 cd $HOME/Projects
 echo "Cloning Github Repositorys, datastructure config javaclub pythonclub"
-git clone https://github.com/diegodila/datastructure.git
+git clone https://github.com/diegodila/cloudplatform.git
 git clone https://github.com/diegodila/config.git
+git clone https://github.com/diegodila/database.git
+git clone https://github.com/diegodila/datastructure.git
+git clone https://github.com/diegodila/fiap.git
+git clone https://github.com/diegodila/governance.git
 git clone https://github.com/diegodila/javaclub.git
 git clone https://github.com/diegodila/pythonclub.git
+
+cd $HOME/Projects/cloudplatform
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/cloudplatform.git
+echo "Show Git remotes"
+git remote -v
+
 cd $HOME/Projects/config
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -31,6 +44,15 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/config.git
 echo "Show Git remotes"
 git remote -v
+
+cd $HOME/Projects/database
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/database.git
+echo "Show Git remotes"
+git remote -v
+
 cd $HOME/Projects/datastructure
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -38,6 +60,32 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
 echo "Show Git remotes"
 git remote -v
+
+cd $HOME/Projects/datastructure
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/fiap
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/fiap.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/governance
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/governance.git
+echo "Show Git remotes"
+git remote -v
+
+
 cd $HOME/Projects/javaclub
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -45,6 +93,7 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/javaclub.git
 echo "Show Git remotes"
 git remote -v
+
 cd $HOME/Projects/pythonclub
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
