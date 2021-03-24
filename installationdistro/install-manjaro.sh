1fc8d95d33939638e5e4a0bec8e019a8d4c34e9a
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-03-24 20:47:29 -0300

    add install-manjaro.sh

--- installationdistro/install-manjaro.sh
+++ installationdistro/install-manjaro.sh
@@ -1,6 +1,5 @@
-echo "Installation Config Linux"
+echo "Installation packages and repositorys Manjaro Linux"
 
-chmod 555 $HOME/Downloads/install.sh
 echo "Configuring Git, global username and email and credetial"
 git config --global user.name "Diego Ferreira Gonçalves"
 git config --global user.email "ferreira.dfg@gmail.com"
@@ -9,10 +8,23 @@ git config --global credential.helper 'cache --timeout=172800'
 mkdir $HOME/Projects
 cd $HOME/Projects
 echo "Cloning Github Repositorys, datastructure config javaclub pythonclub"
-git clone https://github.com/diegodila/datastructure.git
+git clone https://github.com/diegodila/cloudplatform.git
 git clone https://github.com/diegodila/config.git
+git clone https://github.com/diegodila/database.git
+git clone https://github.com/diegodila/datastructure.git
+git clone https://github.com/diegodila/fiap.git
+git clone https://github.com/diegodila/governance.git
 git clone https://github.com/diegodila/javaclub.git
 git clone https://github.com/diegodila/pythonclub.git
+
+cd $HOME/Projects/cloudplatform
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/cloudplatform.git
+echo "Show Git remotes"
+git remote -v
+
 cd $HOME/Projects/config
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -20,6 +32,15 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/config.git
 echo "Show Git remotes"
 git remote -v
+
+cd $HOME/Projects/database
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/database.git
+echo "Show Git remotes"
+git remote -v
+
 cd $HOME/Projects/datastructure
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -27,6 +48,31 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
 echo "Show Git remotes"
 git remote -v
+
+cd $HOME/Projects/datastructure
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/fiap
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/fiap.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/governance
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/governance.git
+echo "Show Git remotes"
+git remote -v
+
 cd $HOME/Projects/javaclub
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -34,6 +80,7 @@ echo "Configuring Repository to add new url push"
 git remote set-url --add origin https://gitlab.com/diegodila/javaclub.git
 echo "Show Git remotes"
 git remote -v
+
 cd $HOME/Projects/pythonclub
 echo "Configuring Repository to pull rebase true"
 git config pull.rebase true
@@ -42,28 +89,18 @@ git remote set-url --add origin https://gitlab.com/diegodila/pythonclub.git
 echo "Show Git remotes"
 git remote -v
 
-mkdir $HOME/.config/alacritty
-cp $HOME/Projects/config/alacritty/alacritty.yml $HOME/.config/alacritty/
-
-echo "Configuring Alacritty terminal, theme and keybindings"
-mkdir $HOME/.config/alacritty
-cp $HOME/Projects/config/alacritty/alacritty.yml $HOME/.config/alacritty/
-echo "Finish"
-
-echo "Configuring theme cinnamon top panel, transparency 40%"
-cp -r /usr/share/themes/Mint-Y-Aqua $HOME/.themes/
-rm $HOME/.themes/Mint-Y-Aqua/cinnamon/cinnamon.css
-cp $HOME/Projects/config/cinnamon/cinnamon.css $HOME/.themes/Mint-Y-Aqua/cinnamon/
-
 echo "Applications Installating"
-sudo pacman -Syu 
-yay
-yay -S papirus-icon-theme arc-gtk-theme ttf-meslo-nerd-font-powerlevel10k volantes-cursors
-yay -S zsh alacritty tilda plank visual-studio-code-bin discord teams
-yay -S obs-studio insync grub-customizer
+sudo pacman -Syu
+yay -S $(cat $HOME/Projects/config/packages/community)
+yay -S $(cat $HOME/Projects/config/packages/AUR)
 
 echo "Installation extensions vscode"
 cat $HOME/Projects/config/vscode/extensions.txt | xargs -L 1 code --install-extension
+
+echo "Configuring theme cinnamon top panel, transparency 40%"
+cp -r /usr/share/themes/Arc $HOME/.themes/
+rm $HOME/.themes/Arc/cinnamon/cinnamon.css
+cp $HOME/Projects/config/cinnamon/cinnamon.css $HOME/.themes/Arc/cinnamon/
 
 echo "Installation Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -75,23 +112,37 @@ echo "Installation finish Powerlevel10k"
 echo "Configuring Alacritty terminal, theme and keybindings"
 mkdir $HOME/.config/alacritty
 cp $HOME/Projects/config/alacritty/alacritty.yml $HOME/.config/alacritty/
-echo "Finish"
+echo "----------------------------------FINISH----------------------------------"
 
 echo "Installation Intellij Idea"
+wget https://download.jetbrains.com/idea/ideaIC-2020.3.3.tar.gz
 sudo tar -xzf ideaIC-2020.3.2.tar.gz -C /opt
+sudo tar -xzf $HOME/Downloads/ideaIC*.tar.gz -C /opt
 /opt/idea-IC-*/bin/idea.sh
 
+echo "Installation DataGrip IDE"
+wget https://download.jetbrains.com/datagrip/datagrip-2020.3.2.tar.gz
+sudo tar xzf datagrip-*.tar.gz -C /opt/
+sudo tar xzf $HOME/Downloads/datagrip-*.tar.gz -C /opt/
+/opt/datagrip-*/bin/datagrip.sh
+/opt/DataGrip-*/bin/datagrip.sh
+
+echo "Installation Pycharm IDE"
+wget https://download.jetbrains.com/python/pycharm-community-2020.3.4.tar.gz
+sudo tar xzf pycharm-*.tar.gz -C /opt/
+/opt/pycharm-*/bin/pycharm.sh
+
 echo "Installing Eclipse"
-sudo tar -zxvf $HOME/Downloads/eclipse-*.tar.gz -C /opt
-/opt/eclipse-installer/eclipse-inst
+wget -c http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2021-03/R/eclipse-java-2021-03-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+sudo tar -zxvf $HOME/Downloads/eclipse*.tar.gz -C /opt
 echo "Create symbolic link eclipse"
-sudo ln -s $HOME/eclipse/*/eclipse/eclipse /usr/bin/eclipse
+sudo ln -s opt/eclipse/eclipse /usr/bin/eclipse
 echo "[Desktop Entry]
 Encoding=UTF-8
 Name=Eclipse IDE
 Comment=Eclipse IDE
 Exec=/usr/bin/eclipse
-Icon=$HOME/eclipse/*/eclipse/eclipse/icon.xpm
+Icon=/opt/eclipse/icon.xpm
 Categories=Application;Development;Java;IDE
 Version=4.8
 Type=Application
