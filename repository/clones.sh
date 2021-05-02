2f3f4b243a4d592d6ac551c46b4ced35a4ce875c
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-05-02 01:46:38 -0300

    adding new folders

---
+++ repository/clones.sh
@@ -0,0 +1,13 @@
+cd $HOME/Projects
+echo "Cloning Github Repositorys, datastructure config javaclub pythonclub"
+git clone https://github.com/diegodila/cloudplatform.git
+git clone https://github.com/diegodila/config.git
+git clone https://github.com/diegodila/database.git
+git clone https://github.com/diegodila/datastructure.git
+git clone https://github.com/diegodila/fiap.git
+git clone https://github.com/diegodila/flutter.git
+git clone https://github.com/diegodila/governance.git
+git clone https://github.com/diegodila/kotlin.git
+git clone https://github.com/diegodila/javaclub.git
+git clone https://github.com/diegodila/pythonclub.git
+git clone https://github.com/diegodila/springboot.git
---
+++ repository/confrepos.sh
@@ -0,0 +1,95 @@
+cd $HOME/Projects/cloudplatform
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/cloudplatform.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/config
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/config.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/database
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/database.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/datastructure
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/datastructure
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/datastructure.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/fiap
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/fiap.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/flutter
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/flutter.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/governance
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/governance.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/kotlin
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/kotlin.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/javaclub
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/javaclub.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/pythonclub
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/pythonclub.git
+echo "Show Git remotes"
+git remote -v
+
+cd $HOME/Projects/springboot
+echo "Configuring Repository to pull rebase true"
+git config pull.rebase true
+echo "Configuring Repository to add new url push"
+git remote set-url --add origin https://gitlab.com/diegodila/springboot.git
+echo "Show Git remotes"
+git remote -v
