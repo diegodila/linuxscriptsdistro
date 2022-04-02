777ac09211bc28e6d5a62b81c77ea68e4c374635
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-04-02 01:43:01 -0300

    adding improvements to the script

--- main/install-archlinux.sh
+++ main/install-archlinux.sh
@@ -19,7 +19,7 @@ echo
 echo "Creating folder Projects"
 mkdir $HOME/Projects
 cd $HOME/Projects
-git clone https://github.com/diegodila/linuxscriptsdistro.git
+GHUSER=diegodila; curl "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
 
 sudo chown -R diegodila:users $HOME/Projects
 sh $HOME/Projects/linuxscriptsdistro/repositories/clones.sh
@@ -143,3 +143,7 @@ sudo usermod -aG rfkill $USER
 # sudo nano /etc/default/grub
 
 #Os-prober é um pacote que serve para reconhecer outros sistemas instalado na máquina
+
+# git pull all local rep
+# ls | xargs -I{} git -C {} pull
+
