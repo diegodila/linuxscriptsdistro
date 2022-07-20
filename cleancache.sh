bd8f482a29e2506d17f74859a18abb9da87f6edc
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-07-20 19:42:13 -0300

    adding clean cache

---
+++ cleancache.sh
@@ -0,0 +1,39 @@
+yay -Scc --noconfirm
+sudo pacman -Scc --noconfirm
+
+rm -r $HOME/.cache/google-chrome/Default/Cache/Cache_Data/
+rm -r $HOME/.cache/google-chrome/Default/Code\ Cache/js/
+rm -r $HOME/.cache/google-chrome/Default/Code\ Cache/wasm/
+rm -r $HOME/.config/google-chrome/Default/Service\ Worker/CacheStorage
+rm -r $HOME/.config/google-chrome/Default/Service\ Worker/ScriptCache/
+
+rm -r $HOME/.cache/google-chrome/Profile\ 12/Cache/Cache_Data/
+rm -r $HOME/.cache/google-chrome/Profile\ 12/Code\ Cache/js/
+rm -r $HOME/.cache/google-chrome/Profile\ 12/Code\ Cache/wasm/
+rm -r $HOME/.config/google-chrome/Profile\ 12/Service\ Worker/CacheStorage
+rm -r $HOME/.config/google-chrome/Profile\ 12/Service\ Worker/ScriptCache/
+
+rm -r $HOME/.cache/google-chrome/Profile\ 10/Cache/Cache_Data/
+rm -r $HOME/.cache/google-chrome/Profile\ 10/Code\ Cache/js/
+rm -r $HOME/.cache/google-chrome/Profile\ 10/Code\ Cache/wasm/
+rm -r $HOME/.config/google-chrome/Profile\ 10/Service\ Worker/CacheStorage
+rm -r $HOME/.config/google-chrome/Profile\ 10/Service\ Worker/ScriptCache/
+
+rm -r $HOME/.cache/google-chrome/Profile\ 6/Cache/Cache_Data/
+rm -r $HOME/.cache/google-chrome/Profile\ 6/Code\ Cache/js/
+rm -r $HOME/.cache/google-chrome/Profile\ 6/Code\ Cache/wasm/
+rm -r $HOME/.config/google-chrome/Profile\ 6/Service\ Worker/CacheStorage
+rm -r $HOME/.config/google-chrome/Profile\ 6/Service\ Worker/ScriptCache/
+
+
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Default/Code\ Cache/js/
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Default/Cache/Cache_Data/
+
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 5/Code\ Cache/js/
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 5/Cache/Cache_Data/
+
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 3/Code\ Cache/js/
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 3/Cache/Cache_Data/
+
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 4/Code\ Cache/js/
+rm -r $HOME/.cache/BraveSoftware/Brave-Browser/Profile\ 4/Cache/Cache_Data/
